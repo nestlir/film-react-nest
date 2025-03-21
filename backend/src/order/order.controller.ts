003a6e65a36fc3341756d991c761f22fc291169c
Nestlir <cassandreray@gmail.com>
2025-03-21 05:55:52 +0300

    afisha

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,33 +1,18 @@
-import {
-  Controller,
-  Post,
-  Get,
-  Body,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/order.dto';
 
-@Controller('order')
+@Controller('/order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
-  async createOrder(@Body() createOrderDto: CreateOrderDto) {
-    try {
-      return await this.orderService.createOrder(createOrderDto);
-    } catch (error) {
-      console.error(
-        '🔴 Ошибка при создании заказа:',
-        error.message,
-        error.stack,
-      );
-      throw new InternalServerErrorException(error.message);
-    }
-  }
+  async create(@Body() createOrderDto: CreateOrderDto) {
+    const items = await this.orderService.createOrder(createOrderDto);
 
-  @Get()
-  async getAllOrders() {
-    return this.orderService.getAllOrders();
+    return {
+      total: items.length,
+      items,
+    };
   }
 }
